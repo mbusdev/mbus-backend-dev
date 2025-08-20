7deb04c2a056573903a7a1b2f410b6502742492b
Ishan Kumar <littleishan@gmail.com>
2025-08-20 19:08:45 -0400

    merge harvey color backend

--- src/v3.ts
+++ src/v3.ts
@@ -87,7 +87,7 @@ const message = {id: "gradamatation", title: "Congrats Grads 🥳", message: "Co
 
 setInterval(updateBusPositions, 7500);
 setInterval(getSelectableRoutes, 60000);
-setInterval(rebuildGraph, 2 * 60 * 1000);
+setInterval(rebuildGraph, 60 * 1000);
 getSelectableRoutes();
 rebuildGraph(); 
 
