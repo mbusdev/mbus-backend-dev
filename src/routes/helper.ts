2dba4f2b38b608dd273ada74e5924e6206ef2429
Edward Zhang <edwzhang@umich.edu>
2026-07-03 23:00:32 -0700

    feat(api): openapi docs generation

--- src/routes/helper.ts
+++ src/routes/helper.ts
@@ -11,10 +11,9 @@
  *
  * Extra functionality will be added as needed.
  *
+ * TODO: post support
  * TODO: add examples
  * TODO: add tests
- * TODO: use doc info, generate docs
- * TODO: convert path acceptors from express format to openapi format
  */
 
 import express from 'express';
@@ -39,53 +38,162 @@ interface ReflectionInfoRaw {
         router: express.Router,
         pathSuffix: string,
         method: 'get',
-        params: z.ZodType,
-        query: z.ZodType,
+        params: Record<string, z.ZodType>,
+        query: Record<string, z.ZodType>,
         resBody: z.ZodType,
+        summary: string,
+        description: string,
     }>,
 };
 
 interface ReflectionInfo {
     routes: Array<{
-        path: string, method: 'get',
-        params: JSONSchema.BaseSchema, query: JSONSchema.BaseSchema, resBody: JSONSchema.BaseSchema,
+        path: string,
+        method: 'get',
+        params: Record<string, JSONSchema.JSONSchema>,
+        query: Record<string, JSONSchema.JSONSchema>,
+        resBody: JSONSchema.BaseSchema,
+        summary: string,
+        description: string,
     }>,
-    model: JSONSchema.BaseSchema,
+    defs: Record<string, JSONSchema.JSONSchema>,
+    // model: JSONSchema.BaseSchema,
 };
 
+interface OpenAPIGetPath {
+    summary: string,
+    description: string,
+    parameters: Array<{
+        name: string,
+        in: "path" | "query",
+        schema: JSONSchema.JSONSchema,
+        required: boolean,
+    }>
+    responses: {
+        "2XX": {
+            description: "success",
+            content: {
+                "application/json": {
+                    schema: JSONSchema.JSONSchema,
+                }
+            }
+        }
+    }
+}
+
+/** the subset of the openapi format(s) we are concerned with generating */
+interface OpenAPI {
+    openapi: "3.1.2",
+    info: {
+        title: string,
+        version: string,
+    },
+    components: {
+        schemas: Record<string, JSONSchema.JSONSchema>,
+    },
+    paths: Record<string, Record<"get", OpenAPIGetPath>>,
+}
+
 function finalize(info: ReflectionInfoRaw): ReflectionInfo {
+    // replace $def with components/schemas
+    const fixSchema = <T>(s: T): T => {
+        if (typeof s !== 'object' || !s) return s;
+        if ('$ref' in s && typeof s.$ref == 'string')
+            s.$ref = s.$ref.replace('$defs', 'components/schemas');
+        for (const v of Object.values(s)) {
+            fixSchema(v);
+        }
+        return s;
+    };
+
     // TODO: try output first then fallback to input
     const schemaOpts: ToJSONSchemaParams = {
-        reused: 'ref',
+        // reused: 'ref',
         io: 'input',
     }
     const resultRoutes = [];
+
+    // used to get the shared $defs
     const model: Record<string, z.ZodType> = {};
+
     for (const route of info.routes) {
         const basePath = info.routers.find((r) => r.router == route.router)?.route;
         if (basePath == undefined) {
             throw new Error('route has missing base path');
         }
-        const path = basePath + route.pathSuffix;
+        const path = (basePath + route.pathSuffix).replace(/:([A-Za-z0-9_]+)/, "{$1}");
+        const finalParams: Record<string, JSONSchema.JSONSchema> = {};
+        for (const param in route.params) {
+            const zodSchema = route.params[param];
+            model[path + ' params ' + param] = zodSchema;
+            finalParams[param] = fixSchema(zodSchema.toJSONSchema(schemaOpts));
+        }
+        const finalQuery: Record<string, JSONSchema.JSONSchema> = {};
+        for (const key in route.query) {
+            const zodSchema = route.query[key];
+            model[path + '?' + key] = zodSchema;
+            finalQuery[key] = fixSchema(zodSchema.toJSONSchema(schemaOpts));
+        }
         resultRoutes.push({
             path, method: route.method,
-            params: route.params.toJSONSchema(schemaOpts),
-            query: route.query.toJSONSchema(schemaOpts),
-            resBody: route.resBody.toJSONSchema(schemaOpts),
+            params: finalParams,
+            query: finalQuery,
+            resBody: fixSchema(route.resBody.toJSONSchema(schemaOpts)),
+            summary: route.summary,
+            description: route.description,
         });
-        model[path + ' params'] = route.params;
-        model[path + ' query'] = route.query;
         model[path + ' resBody'] = route.resBody;
     }
     return {
         routes: resultRoutes,
-        model: z.object(model).toJSONSchema(schemaOpts),
+        defs: fixSchema(z.object(model).toJSONSchema(schemaOpts)).$defs ?? {},
     };
+}
+
+function makeOpenAPI(info: ReflectionInfo): OpenAPI {
+    const pathsArray = info.routes.map((route) => {
+        const parameters: OpenAPIGetPath['parameters'] = [];
+        for (const name in route.params) {
+            parameters.push({ name: name, in: 'path', required: true, schema: route.params[name] });
+        }
+        for (const name in route.query) {
+            parameters.push({ name: name, in: 'query', required: true, schema: route.query[name] });
+        }
+        const responses: OpenAPIGetPath['responses'] = {
+            '2XX': {
+                description: 'success',
+                content: {
+                    'application/json': { schema: route.resBody }
+                }
+            }
+        };
+        const path: OpenAPIGetPath = {
+            summary: route.summary,
+            description: route.description,
+            parameters,
+            responses,
+        };
+        return { url: route.path, path: { get: path } };
+    });
+    const paths: OpenAPI['paths'] = {};
+    for (const { url, path } of pathsArray) {
+        paths[url] = path;
+    }
+    return {
+        openapi: "3.1.2",
+        info: {
+            title: "Maize Bus Backend",
+            version: "",
+        },
+        components: { schemas: info.defs },
+        paths,
+    }
 }
 
 export function dumpReflectionInfo() {
     const finalized = finalize(info);
-    console.log(JSON.stringify(finalized, null, 4));
+    const openAPI = makeOpenAPI(finalized);
+    console.log(JSON.stringify(openAPI, null, 4));
 }
 
 export function addRouter(app: express.Express, route: string, router: express.Router) {
@@ -93,17 +201,6 @@ export function addRouter(app: express.Express, route: string, router: express.R
         info.routers.push({ route, router });
     }
     app.use(route, router);
-}
-
-export interface GetFormat<
-    P extends z.ZodType,
-    Q extends z.ZodType,
-    RB extends z.ZodType
-> {
-    /** path parameters */
-    params: P,
-    query: Q,
-    resBody: RB,
 }
 
 /**
@@ -116,14 +213,14 @@ export type HandlerReturn<T> = {
 };
 
 /**
- * helper functions that should avoid weird typechecker issues
+ * helper function that should avoid weird typechecker issues
  */
 export function makeSuccessResponse<T>(status: 200 | 201 | 202 | 203 | 205, json: T): HandlerReturn<T> {
     return { success: true, status, json };
 }
 
 /**
- * helper functions that should avoid weird typechecker issues
+ * helper function that should avoid weird typechecker issues
  */
 export function makeFailureResponse<T>(status: 400 | 401 | 403 | 404 | 500, error: string): HandlerReturn<T> {
     return { success: false, status, error };
@@ -137,13 +234,13 @@ export function makeFailureResponse<T>(status: 400 | 401 | 403 | 404 | 500, erro
  * now, the functionality needed will be incorporated
  */
 export function addGetRoute<
-    P extends z.ZodType,
-    Q extends z.ZodType,
+    P extends z.ZodObject<Record<string, z.ZodType>>,
+    Q extends z.ZodObject<Record<string, z.ZodType>>,
     RB extends z.ZodType
 >(
     router: express.Router,
     path: string,
-    format: GetFormat<P, Q, RB>,
+    format: { params: P, query: Q, resBody: RB },
     handler: (params: z.infer<P>, query: z.infer<Q>) => HandlerReturn<z.infer<RB>>,
     docs?: {
         /** a short description of what is route does */
@@ -157,7 +254,8 @@ export function addGetRoute<
     if (reflection) {
         info.routes.push({
             router, method: 'get', pathSuffix: path,
-            params: paramsSchema, query: querySchema, resBody: resBodySchema,
+            params: paramsSchema.shape, query: querySchema.shape, resBody: resBodySchema,
+            summary: docs?.summary ?? "", description: docs?.description ?? "",
         })
     }
 
