838043ce4167cf0e464975b82732d6daa36d8566
Edward Zhang <edwzhang@umich.edu>
2026-07-22 13:13:57 -0700

    feat: test stubs, output to file, flesh out tsdoc comments

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import express from "express";
 
 import mbus from "./routes/api"
-import { addRouter, dumpReflectionInfo, reflection } from "./routes/documented";
+import * as documented from "./routes/documented";
 
 const app = express();
 
 app.use(express.json());
-addRouter(app, "/mbus/api/v3", mbus);
+documented.addRouter(documented.globalContext, app, "/mbus/api/v3", mbus);
 app.use("/docs", express.static("docs"));
 
 const PORT = process.env.PORT || 3000;
@@ -14,6 +14,7 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-    if (reflection)
-        dumpReflectionInfo();
+    if (documented.ENABLED) {
+        documented.outputDocsFor(documented.globalContext);
+    }
 });
